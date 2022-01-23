09706060319070a5d0ca998309a4674cacfd7e7e
web-yuma <94731528+web-yuma@users.noreply.github.com>
2022-01-23 16:48:12 +0900

    Create index.js

---
+++ index.js
@@ -0,0 +1,10 @@
+$('.slider').slick({
+    autoplay:true,
+    autoplaySpeed:5000,
+    dots:true,
+	arrows:false,
+	centerMode:true,
+	centerPadding:'80px',
+	slidesToShow: 1,
+	pauseOnHover:false,
+});
